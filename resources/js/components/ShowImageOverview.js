3416af6f5110c92c74f784897344b35ff42bfce0
unknown <ola.ses@yahoo.com>
2023-03-26 22:09:14 +0100

    Add react image preview to post

---
+++ resources/js/components/ShowImageOverview.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import ReactDOM from 'react-dom';
+
+function ShowImageOverview() {
+    const [file, setFile] = useState();
+  
+    function handleChange(e) {
+       
+        setFile(URL.createObjectURL(e.target.files[0]));
+        
+    }
+  
+    return (
+        <>
+            <input type="file"  name="image" className="file-upload-default "  onChange={handleChange}/>
+            <img src={file} style={{height: "100px", marginTop:"10px"}}/>
+        </>
+  
+    );
+}
+
+
+export default ShowImageOverview;
+if (document.getElementById('showimageoverview')) {
+    ReactDOM.render(<ShowImageOverview />, document.getElementById('showimageoverview'));
+}
